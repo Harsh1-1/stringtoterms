36d6d98125d377097ab81745b8943e2112c4d6a7
harsh.ku <harsh.ku@media.net>
2018-09-27 12:54:01 +0530

    gitignore files added

--- stringtoterms/stringtoterms.py
+++ stringtoterms/stringtoterms.py
@@ -7,18 +7,18 @@ stemmer = Porter2Stemmer()
 #function to get the list of all terms from one doc
 def stringtoterms(string, stemming=False):
 
-	#for refining input content
-	delimiters = " ",".","(c)","?","-","#","*","_","/","(",")","!","'",'"',",",";",":","&","<",">","\r","\n","\t"
-	regexPattern = '|'.join(map(re.escape, delimiters))
-	refinedlist = re.split(regexPattern, string)
+    #for refining input content
+    delimiters = " ",".","(c)","?","-","#","*","_","/","(",")","!","'",'"',",",";",":","&","<",">","\r","\n","\t"
+    regexPattern = '|'.join(map(re.escape, delimiters))
+    refinedlist = re.split(regexPattern, string)
 
-	termlist = []
+    termlist = []
 
-	for word in refinedlist:
+    for word in refinedlist:
         if stemming == True:
-		          word = stemmer.stem(word)
-		termlist.append(word)
+            word = stemmer.stem(word)
+        termlist.append(word)
 
-	termlist = filter(None,termlist)
-	#print termlist
-	return list(termlist)
+    termlist = filter(None,termlist)
+    #print termlist
+    return list(termlist)
